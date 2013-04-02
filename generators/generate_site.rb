d9aceecb8cf8b2c4e42951cf890a48024377d371
C S <cs@Cs-MacBook-Air.(none)>
2013-04-01 20:42:35 -0400

    Added Site Generator Script

---
+++ generators/generate_site.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'erb'
+site_template_list ="site_template.erb"
+@title=nil
+@items=[]
+@is_list=true
+# the content directory drives the content for the site
+Dir.new('content').entries.grep(/.*.txt/).each do |note|
+  
+   tags = note.split('_')
+   dt = tags.shift
+   dt= "#{dt[0,4]}-#{dt[4,2]}-#{dt[6,2]}"
+   content = File.open("content/#{note}").readlines.join("\n")
+   tags.map{|m|m.gsub!('.txt','')}
+   first_sentence = content.split('.  ')[0]
+   first_sentence += " <a href='#{dt}.html'>[...]</a>"
+   @items << {:tags=>tags, 
+              :date=>"#{dt}", 
+              :content=>content, 
+              :first_sentence => first_sentence
+              }
+   
+end
+
+htmls = Dir.entries('..').grep(/.*.html/)
+idx = htmls.delete('index.html')
+about = htmls.delete('about.html')
+# index
+File.open("../#{idx}",'w'){|f|f.puts ERB.new(File.read(site_template_list)).result} 
+
+# tags page
+@all_items = @items
+htmls.each{|html|
+   @title=html.gsub('.html','')
+   puts "Processing #{@title}"
+   @items=@all_items.find_all{|i|i[:tags].include?(@title)}
+   puts "Total #{@items.size}"   
+   File.open("../#{html}",'w'){|f|f.puts ERB.new(File.read(site_template_list)).result} unless @items.nil?      
+}
+
+# details
+@is_list=false
+@all_items.each{|item|
+  puts "Detail #{item[:date]}"
+   @item=item
+   File.open("../#{item[:date]}.html",'w'){|f|f.puts ERB.new(File.read(site_template_list)).result}
+}
